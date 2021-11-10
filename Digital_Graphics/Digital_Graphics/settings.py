7f81ee1de2a4ffa14b5c46e3345fefeb8eacbd75
unknown <8bpcoins4u@gmail.com>
2021-11-10 22:50:42 +0500

    Few Changes

--- Digital_Graphics/Digital_Graphics/settings.py
+++ Digital_Graphics/Digital_Graphics/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t$_!(u99)4l$h-i2ntih7#ok_)yu2ows904w%wd!t6n0jaj391'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost:8000', '127.0.0.1:8000',
+                 'https://wwww.digital-graphics.herokuapp.com']
 
 
 # Application definition
